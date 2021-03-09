43fc2a2202e1e08f57d80f97086dc6d18a034eb6
s1871055@s.konan-u.ac.jp <s1871055@s.konan-u.ac.jp>
2021-03-09 13:51:02 +0900

    add comments

--- src/CommandManager.py
+++ src/CommandManager.py
@@ -1,8 +1,7 @@
 # コマンドを押したり引いたりすることを感知する
 """
-
 参考:
-ASCII https://theasciicode.com.ar/ascii-control-characters/escape-ascii-code-27.html
+ASCII CODE https://theasciicode.com.ar/ascii-control-characters/escape-ascii-code-27.html
 """
 
 from msvcrt import getch
@@ -12,9 +11,11 @@ class CommandManager:
     def __init__(self):
         """
         ASCIIコードでKeyを指定
-        {ASCII:押されたときの名前}
+        {ASCII code : 押されたときの名前}
         """
 
+        # コマンドを増やしたいとき増やしてください
+        # key: ascii code, value: command name (user definition)
         self.setKey = {
             119: "w",
             115: "s",
@@ -30,11 +31,15 @@ class CommandManager:
         キーボードを押したときに実行される関数
         @return:
             key name， e.g. [e]キーを押されたときは"e"と返る．
-            変数: setKeyで登録されていない場合は"NOT_SET_KEY"と返る．
+            変数: setKeyで登録されていない場合はNoneが返る．
         """
-        key = ord(getch())
+        key = ord(getch()) #文字をUnicodeに変換
 
         if key in self.setKey:
             return self.setKey[key]
         else:
-            return "NOT_SET_KEY"
+            print("Warning: setKeyで登録されていないキータイプです．(CommandManager.py, pressKey())")
+            return None
+
+if __name__ == "__main__":
+    print(CommandManager().pressKey()) #pressKeyでinputと同じく止まる．一文字のみのキーを感知する．
